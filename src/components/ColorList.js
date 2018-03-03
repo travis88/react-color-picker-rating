29a55786cfc6fcd116d561fb449b430511058072
travis88 <hellsballs99@gmail.com>
2018-03-03 17:16:36 +0300

    initial commit

---
+++ src/components/ColorList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Color from './Color';
+
+const ColorList = ({ colors=[], onRate=f=>f, onRemove=f=>f }) =>
+    <div className="color-list">
+        {(colors.length === 0) ? 
+            <p>No colors listed. (Add a color)</p> :
+            colors.map(color =>
+                <Color key={color.id} 
+                       {...color}
+                       onRate={(rating) => onRate(color.id, rating)}
+                       onRemove={() => onRemove(color.id)} />
+            )
+        }
+    </div>
+
+export default ColorList;
---
+++ src/components/StarRating.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Star from './Star';
+
+const StarRating = ({starsSelected=0, totalStars=5, onRate=f=>f}) => 
+    <div className="star-rating">
+        {[...Array(totalStars)].map((n, i) =>
+            <Star key={i} 
+                  selected={i < starsSelected}
+                  onClick={() => onRate(i + 1)} />
+        )}
+        <br />
+        <p>{starsSelected} of {totalStars} stars</p>
+    </div>
+
+StarRating.propTypes = {
+    totalStars: PropTypes.number
+}
+
+StarRating.defaultProps = {
+    totalStars: 5
+}
+
+export default StarRating;
